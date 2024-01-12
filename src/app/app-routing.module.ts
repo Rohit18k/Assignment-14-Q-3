caeae046078909ffbbe5f9e0573990e5418636d3
Rohit_Kakade <rohit08kakade@gmail.com>
2024-01-12 12:16:09 +0530

    This is Assignment 14 Question 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { BooksComponent } from './books/books.component';
+import { WrongChoiceComponent } from './wrong-choice/wrong-choice.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'technologies', component: TechnologiesComponent },
+  { path: 'books', component: BooksComponent },
+  { path: 'wrong-choice', component: WrongChoiceComponent },
+  { path: '', redirectTo: '/technologies', pathMatch: 'full' },
+  { path: '**', redirectTo: '/wrong-choice' }, // Default route for wrong choices
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { BooksComponent } from './books/books.component';
+import { WrongChoiceComponent } from './wrong-choice/wrong-choice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TechnologiesComponent,
+    BooksComponent,
+    WrongChoiceComponent
   ],
   imports: [
     BrowserModule,
